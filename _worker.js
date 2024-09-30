3b524efcccf54a8552e304533e235874b043b053
behbodrezai <138313966+behbodrezai@users.noreply.github.com>
2024-09-30 23:22:06 +0330

    holand.js

--- _worker.js
+++ _worker.js
@@ -7,10 +7,10 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '088f231b-e466-436b-a4a6-3f4b8d7f2fb8';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['158.101.209.76'];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
